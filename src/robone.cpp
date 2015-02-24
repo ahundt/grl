10176c07bd4426e7c39aac1473efd46f91c9f91e
Andrew Hundt <ATHundt@gmail.com>
2015-02-24 00:15:19 -0500

    #51 first pass at proper implementation of flat buffers generation with cmake

---
+++ src/robone.cpp
@@ -0,0 +1,42 @@
+
+// Library includes
+#include <string>
+#include <ostream>
+#include <iostream>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/math/special_functions/binomial.hpp>
+#include <boost/filesystem.hpp>
+#include <cmath>
+#include <thread>
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+#include <Eigen/Eigenvalues>
+#include <Eigen/Geometry>
+
+
+namespace po = boost::program_options;
+
+
+
+/**************************************************************************/
+/**
+ * @brief Main function
+ *
+ * @param argc  Number of input arguments
+ * @param argv  Pointer to input arguments
+ *
+ * @return int
+ */
+int main(int argc,char**argv) {
+
+	
+	return 0;
+}
+
+
+
