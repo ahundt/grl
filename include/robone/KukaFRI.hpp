0a094ba300d3a5b9c72576c3ccff82eb874f8885
Andrew Hundt <ATHundt@gmail.com>
2015-03-04 21:04:45 -0500

    #12 INTERMEDIATE CODE: simplified API by directly using MonitoringMessage rather than requiring KUKA::FRI::ClientData

--- include/robone/KukaFRI.hpp
+++ include/robone/KukaFRI.hpp
@@ -48,74 +48,74 @@ namespace robone { namespace robot {
 	
 	/// copy measured joint angle to output iterator
 	template<typename OutputIterator>
-	void copy(const KUKA::FRI::ClientData& friData, OutputIterator it, boost::units::plane_angle_base_dimension, robone::robot::state_tag){
+	void copy(const FRIMonitoringMessage& monitoringMsg, OutputIterator it, boost::units::plane_angle_base_dimension, robone::robot::state_tag){
 
-        if (friData.monitoringMsg.monitorData.has_measuredJointPosition) {
-           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(friData.monitoringMsg.monitorData.measuredJointPosition.value.arg),it);
+        if (monitoringMsg.monitorData.has_measuredJointPosition) {
+           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(monitoringMsg.monitorData.measuredJointPosition.value.arg),it);
         }
 	}
 	
 	
 	/// copy commanded joint angle to output iterator
 	template<typename OutputIterator>
-	void copy(const KUKA::FRI::ClientData& friData, OutputIterator it, boost::units::plane_angle_base_dimension, robone::robot::command_tag){
+	void copy(const FRIMonitoringMessage& monitoringMsg, OutputIterator it, boost::units::plane_angle_base_dimension, robone::robot::command_tag){
 
-        if (friData.monitoringMsg.monitorData.has_commandedJointPosition) {
-           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(friData.monitoringMsg.monitorData.commandedJointPosition.value.arg),it);
+        if (monitoringMsg.monitorData.has_commandedJointPosition) {
+           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(monitoringMsg.monitorData.commandedJointPosition.value.arg),it);
         }
 	}
 	
 	
 	/// copy measured joint torque to output iterator
 	template<typename OutputIterator>
-	void copy(const KUKA::FRI::ClientData& friData, OutputIterator it, boost::units::torque_dimension, robone::robot::state_tag){
-        if (friData.monitoringMsg.monitorData.has_measuredTorque) {
-           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(friData.monitoringMsg.monitorData.measuredTorque.value.arg),it);
+	void copy(const FRIMonitoringMessage& monitoringMsg, OutputIterator it, boost::units::torque_dimension, robone::robot::state_tag){
+        if (monitoringMsg.monitorData.has_measuredTorque) {
+           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(monitoringMsg.monitorData.measuredTorque.value.arg),it);
         }
 	}
 	
 	/// copy measured external joint torque to output iterator
 	template<typename OutputIterator>
-	void copy(const KUKA::FRI::ClientData& friData, OutputIterator it, boost::units::torque_dimension, external_state_tag){
-        if (friData.monitoringMsg.monitorData.has_externalTorque) {
-           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(friData.monitoringMsg.monitorData.externalTorque.value.arg),it);
+	void copy(const FRIMonitoringMessage& monitoringMsg, OutputIterator it, boost::units::torque_dimension, external_state_tag){
+        if (monitoringMsg.monitorData.has_externalTorque) {
+           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(monitoringMsg.monitorData.externalTorque.value.arg),it);
         }
 	}
 	
 	/// copy commanded  joint torque to output iterator
 	template<typename OutputIterator>
-	void copy(const KUKA::FRI::ClientData& friData, OutputIterator it, boost::units::torque_dimension, command_tag){
-        if (friData.monitoringMsg.monitorData.has_commandedTorque) {
-           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(friData.monitoringMsg.monitorData.commandedTorque.value.arg),it);
+	void copy(const FRIMonitoringMessage& monitoringMsg, OutputIterator it, boost::units::torque_dimension, command_tag){
+        if (monitoringMsg.monitorData.has_commandedTorque) {
+           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(monitoringMsg.monitorData.commandedTorque.value.arg),it);
         }
 	}
 	
 	/// copy interpolated commanded joint angles
 	template<typename OutputIterator>
-	void copy(const KUKA::FRI::ClientData& friData, OutputIterator it,boost::units::plane_angle_base_dimension,interpolated_state_tag){
-        if (friData.monitoringMsg.ipoData.has_jointPosition) {
-           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(friData.monitoringMsg.ipoData.jointPosition.value.arg),it);
+	void copy(const FRIMonitoringMessage& monitoringMsg, OutputIterator it,boost::units::plane_angle_base_dimension,interpolated_state_tag){
+        if (monitoringMsg.ipoData.has_jointPosition) {
+           kuka::detail::copyJointState(static_cast<tRepeatedDoubleArguments*>(monitoringMsg.ipoData.jointPosition.value.arg),it);
         }
 
 	}
 	
 	/// @todo consider changing these get* functions to get(data,type_tag());
-	KUKA::FRI::ESafetyState getSafetyState(const KUKA::FRI::ClientData& friData) {
+	KUKA::FRI::ESafetyState getSafetyState(const FRIMonitoringMessage& monitoringMsg) {
 		KUKA::FRI::ESafetyState KukaSafetyState = KUKA::FRI::ESafetyState::NORMAL_OPERATION;
-	    if (friData.monitoringMsg.has_robotInfo) {
-	        if (friData.monitoringMsg.robotInfo.has_safetyState)
-	            KukaSafetyState = static_cast<KUKA::FRI::ESafetyState>(friData.monitoringMsg.robotInfo.safetyState);
+	    if (monitoringMsg.has_robotInfo) {
+	        if (monitoringMsg.robotInfo.has_safetyState)
+	            KukaSafetyState = static_cast<KUKA::FRI::ESafetyState>(monitoringMsg.robotInfo.safetyState);
 	    }
 		return KukaSafetyState;
 	}
 	
 	
 	/// @todo consider changing these get* functions to get(data,type_tag());
-	KUKA::FRI::EOperationMode getOperationMode(const KUKA::FRI::ClientData& friData) {
+	KUKA::FRI::EOperationMode getOperationMode(const FRIMonitoringMessage& monitoringMsg) {
 		KUKA::FRI::EOperationMode KukaSafetyState = KUKA::FRI::EOperationMode::TEST_MODE_1;
-	    if (friData.monitoringMsg.has_robotInfo) {
-	        if (friData.monitoringMsg.robotInfo.has_operationMode)
-	            KukaSafetyState = static_cast<KUKA::FRI::EOperationMode>(friData.monitoringMsg.robotInfo.operationMode);
+	    if (monitoringMsg.has_robotInfo) {
+	        if (monitoringMsg.robotInfo.has_operationMode)
+	            KukaSafetyState = static_cast<KUKA::FRI::EOperationMode>(monitoringMsg.robotInfo.operationMode);
 	    }
 		return KukaSafetyState;
 	}
@@ -123,68 +123,68 @@ namespace robone { namespace robot {
 	
 	/// @todo consider changing these get* functions to get(data,type_tag());
 		/// @todo this one requires a callback... figure it out
-	KUKA::FRI::EDriveState getDriveState(const KUKA::FRI::ClientData& friData) {
+	KUKA::FRI::EDriveState getDriveState(const FRIMonitoringMessage& monitoringMsg) {
 		KUKA::FRI::EDriveState KukaSafetyState = KUKA::FRI::EDriveState::OFF;
-	    if (friData.monitoringMsg.has_robotInfo) {
-	         //   KukaSafetyState = static_cast<KUKA::FRI::EDriveState>(friData.monitoringMsg.robotInfo.driveState);
+	    if (monitoringMsg.has_robotInfo) {
+	         //   KukaSafetyState = static_cast<KUKA::FRI::EDriveState>(monitoringMsg.robotInfo.driveState);
 	    }
 		return KukaSafetyState;
 	}
 	
 #if 0 // original getConnectionInfo
-	getConnectionInfo(const KUKA::FRI::ClientData& friData){
+	getConnectionInfo(const FRIMonitoringMessage& monitoringMsg){
 
-	    if (friData.monitoringMsg.has_connectionInfo) {
-	        KukaSessionState = friData.monitoringMsg.connectionInfo.sessionState;
-	        KukaQuality = friData.monitoringMsg.connectionInfo.quality;
-	        if (friData.monitoringMsg.connectionInfo.has_sendPeriod)
-	            KukaSendPeriod = friData.monitoringMsg.connectionInfo.sendPeriod;
-	        if (friData.monitoringMsg.connectionInfo.has_receiveMultiplier)
-	            KukaReceiveMultiplier = friData.monitoringMsg.connectionInfo.receiveMultiplier;
+	    if (monitoringMsg.has_connectionInfo) {
+	        KukaSessionState = monitoringMsg.connectionInfo.sessionState;
+	        KukaQuality = monitoringMsg.connectionInfo.quality;
+	        if (monitoringMsg.connectionInfo.has_sendPeriod)
+	            KukaSendPeriod = monitoringMsg.connectionInfo.sendPeriod;
+	        if (monitoringMsg.connectionInfo.has_receiveMultiplier)
+	            KukaReceiveMultiplier = monitoringMsg.connectionInfo.receiveMultiplier;
 	    }
 	}
 #endif
 	
-	KUKA::FRI::ESessionState getSessionState(const KUKA::FRI::ClientData& friData){
+	KUKA::FRI::ESessionState getSessionState(const FRIMonitoringMessage& monitoringMsg){
 		KUKA::FRI::ESessionState KukaSessionState = KUKA::FRI::ESessionState::IDLE;
-	    if (friData.monitoringMsg.has_connectionInfo) {
-	        KukaSessionState = static_cast<KUKA::FRI::ESessionState>(friData.monitoringMsg.connectionInfo.sessionState);
+	    if (monitoringMsg.has_connectionInfo) {
+	        KukaSessionState = static_cast<KUKA::FRI::ESessionState>(monitoringMsg.connectionInfo.sessionState);
 	    }
 		return KukaSessionState;
 	}
 	
-	KUKA::FRI::EConnectionQuality getConnectionQuality(const KUKA::FRI::ClientData& friData){
+	KUKA::FRI::EConnectionQuality getConnectionQuality(const FRIMonitoringMessage& monitoringMsg){
 		KUKA::FRI::EConnectionQuality KukaQuality = KUKA::FRI::EConnectionQuality::POOR;
-	    if (friData.monitoringMsg.has_connectionInfo) {
-	        KukaQuality = static_cast<KUKA::FRI::EConnectionQuality>(friData.monitoringMsg.connectionInfo.quality);
+	    if (monitoringMsg.has_connectionInfo) {
+	        KukaQuality = static_cast<KUKA::FRI::EConnectionQuality>(monitoringMsg.connectionInfo.quality);
 	    }
 		return KukaQuality;
 	}
 	
-	uint32_t getSendPeriod(const KUKA::FRI::ClientData& friData){
+	uint32_t getSendPeriod(const FRIMonitoringMessage& monitoringMsg){
 		uint32_t KukaSendPeriod = 0;
-	    if (friData.monitoringMsg.has_connectionInfo) {
-	        if (friData.monitoringMsg.connectionInfo.has_sendPeriod)
-	            KukaSendPeriod = friData.monitoringMsg.connectionInfo.sendPeriod;
+	    if (monitoringMsg.has_connectionInfo) {
+	        if (monitoringMsg.connectionInfo.has_sendPeriod)
+	            KukaSendPeriod = monitoringMsg.connectionInfo.sendPeriod;
 	    }
 		return KukaSendPeriod;
 	}
 	
-	std::size_t getReceiveMultiplier(const KUKA::FRI::ClientData& friData){
+	std::size_t getReceiveMultiplier(const FRIMonitoringMessage& monitoringMsg){
 		std::size_t KukaReceiveMultiplier = 0;
-	    if (friData.monitoringMsg.has_connectionInfo) {
-	        if (friData.monitoringMsg.connectionInfo.has_receiveMultiplier)
-	            KukaReceiveMultiplier = friData.monitoringMsg.connectionInfo.receiveMultiplier;
+	    if (monitoringMsg.has_connectionInfo) {
+	        if (monitoringMsg.connectionInfo.has_receiveMultiplier)
+	            KukaReceiveMultiplier = monitoringMsg.connectionInfo.receiveMultiplier;
 	    }
 		return KukaReceiveMultiplier;
 	}
 	
-	std::chrono::time_point<std::chrono::high_resolution_clock> getTimeStamp(const KUKA::FRI::ClientData& friData){
+	std::chrono::time_point<std::chrono::high_resolution_clock> getTimeStamp(const FRIMonitoringMessage& monitoringMsg){
 		// defaults to the epoch
 		std::chrono::time_point<std::chrono::high_resolution_clock> timestamp;
-        if (friData.monitoringMsg.monitorData.has_timestamp) {
-            timestamp += std::chrono::seconds(friData.monitoringMsg.monitorData.timestamp.sec) +
-				         std::chrono::nanoseconds(friData.monitoringMsg.monitorData.timestamp.nanosec);
+        if (monitoringMsg.monitorData.has_timestamp) {
+            timestamp += std::chrono::seconds(monitoringMsg.monitorData.timestamp.sec) +
+				         std::chrono::nanoseconds(monitoringMsg.monitorData.timestamp.nanosec);
         }
 		return timestamp;
 	}
@@ -251,18 +251,18 @@ namespace robone { namespace robot {
 	            return;
 	}
 	
-	void copy(const KUKA::FRI::ClientData& friData, KukaState& state ){
-		copy(friData,state.position.begin(),boost::units::plane_angle_base_dimension(),robone::robot::state_tag());
-		copy(friData,state.torque.begin(),boost::units::torque_dimension(),robone::robot::state_tag());
-		copy(friData,state.commandedPosition.begin(),boost::units::plane_angle_base_dimension(),robone::robot::command_tag());
-		copy(friData,state.commandedTorque.begin(),boost::units::torque_dimension(),robone::robot::command_tag());
-		copy(friData,state.ipoJointPosition.begin(),boost::units::plane_angle_base_dimension(),robone::robot::interpolated_state_tag());
-		state.sessionState = getSessionState(friData);
-		state.connectionQuality = getConnectionQuality(friData);
-		state.safetyState = getSafetyState(friData);
-		state.operationMode = getOperationMode(friData);
+	void copy(const FRIMonitoringMessage& monitoringMsg, KukaState& state ){
+		copy(monitoringMsg,state.position.begin(),boost::units::plane_angle_base_dimension(),robone::robot::state_tag());
+		copy(monitoringMsg,state.torque.begin(),boost::units::torque_dimension(),robone::robot::state_tag());
+		copy(monitoringMsg,state.commandedPosition.begin(),boost::units::plane_angle_base_dimension(),robone::robot::command_tag());
+		copy(monitoringMsg,state.commandedTorque.begin(),boost::units::torque_dimension(),robone::robot::command_tag());
+		copy(monitoringMsg,state.ipoJointPosition.begin(),boost::units::plane_angle_base_dimension(),robone::robot::interpolated_state_tag());
+		state.sessionState = getSessionState(monitoringMsg);
+		state.connectionQuality = getConnectionQuality(monitoringMsg);
+		state.safetyState = getSafetyState(monitoringMsg);
+		state.operationMode = getOperationMode(monitoringMsg);
 		/// @todo this one requires a callback... figure it out
-		//state.driveState = getDriveState(friData);
+		//state.driveState = getDriveState(monitoringMsg);
 			
 		/// @todo fill out missing state update steps
 		
@@ -273,7 +273,7 @@ namespace robone { namespace robot {
 	/// @todo implement sending state
 	void update_state(boost::asio::ip::udp::socket& socket, KUKA::FRI::ClientData& friData, KukaState& state){
 		std::size_t buf_size = socket.receive(boost::asio::buffer(friData.receiveBuffer,KUKA::FRI::FRI_MONITOR_MSG_MAX_SIZE));
-		copy(friData,state);
+		copy(friData.monitoringMsg,state);
 
 	    friData.lastSendCounter++;
 	    // Check whether to send a response
