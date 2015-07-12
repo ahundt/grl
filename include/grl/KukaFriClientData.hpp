2794b938ecf25ccd0361d3a7889ccee585df3e09
Andrew Hundt <ATHundt@gmail.com>
2015-07-12 15:09:36 -0400

    Created basic driver for KukaFriClientData synchronous API and separated out some common shared functions to avoid redundancy where possible.

--- include/grl/KukaFriClientData.hpp
+++ include/grl/KukaFriClientData.hpp
@@ -5,6 +5,7 @@
 #include <boost/range/algorithm/transform.hpp>
 #include "friClientData.h"
 #include "grl/KukaFRI.hpp"
+#include "grl/KukaFRIThreadSeparator.hpp"
 #include <boost/log/trivial.hpp>
 
 /// @todo move back to KukaFRIClientDataTest.cpp
@@ -88,10 +89,6 @@ void decode(KUKA::FRI::ClientData& friData, std::size_t msg_size){
 //        set(friData.commandMsg, state.commandedPosition, grl::revolute_joint_angle_open_chain_command_tag());
 //    }
 
-
-
-
-
 /// encode data in the class into the send buffer
 std::size_t encode(KUKA::FRI::ClientData& friData,boost::system::error_code& ec){
     // reset send counter
@@ -128,13 +125,208 @@ void update_state(boost::asio::ip::udp::socket& socket, KUKA::FRI::ClientData& f
 }
 
 
+class KukaFRIClientDataDriver : public std::enable_shared_from_this<KukaFRIClientDataDriver>, public KukaFRI {
+    
+public:
+
+    using KukaFRI::ParamIndex;
+    using KukaFRI::ThreadingRunMode;
+    using KukaFRI::Params;
+    using KukaFRI::defaultParams;
+    
+    
+    
+	KukaFRIClientDataDriver(boost::asio::io_service& ios, Params params = defaultParams())
+        :
+        isConnectionEstablished_(false),
+        io_service_(ios)
+    {
+      construct(params);
+	}
+    
+    
+	KukaFRIClientDataDriver(Params params = defaultParams())
+        :
+        isConnectionEstablished_(false),
+        optional_internal_io_service_P(new boost::asio::io_service),
+        io_service_(*optional_internal_io_service_P)
+    {
+        construct(params);
+	}
+
+    /// Call this to initialize the object
+    void construct(Params params = defaultParams())
+    {
+
+        try
+        {
+            connect(params, io_service_);
+            // start up the driver thread since the io_service_ is internal only
+            if(std::get<is_running_automatically>(params))
+            {
+              driver_threadP_.reset(new std::thread([&]{ update(); }));
+            }
+        
+        } catch( boost::exception &e) {
+            add_details_to_connection_error(e,params);
+            throw;
+        }
+        
+    }
+    
+    /// @brief blocking call to communicate with the robot continuously
+    /// @pre construct() should
+    void run(){
+      update();
+    }
+
+    /// @brief Updates the passed friData shared pointer to a pointer to newly updated data, plus any errors that occurred.
+    ///
+    /// @note This function is not thread safe cannot be called simultaneously from multiple threads.
+    ///
+    /// @param[in,out] friData the pointer to update with new state and optional input state.
+    ///
+    /// @pre If friData!=nullptr it is assumed valid for use and this class will take control of the object.
+    void update_state(std::shared_ptr<KUKA::FRI::ClientData>& friData, boost::system::error_code& receive_ec,std::size_t& receive_bytes_transferred, boost::system::error_code& send_ec, std::size_t& send_bytes_transferred){
+       
+        
+        // ensure we have valid data for future updates
+        std::shared_ptr<KUKA::FRI::ClientData> tempFriData;
+        if(friData.get() == nullptr)
+        {
+          tempFriData = std::make_shared<KUKA::FRI::ClientData>(KUKA::LBRState::NUM_DOF);
+        }
+        else
+        {
+          tempFriData = friData;
+        }
+        std::atomic_exchange(&userThreadStateP_,latestStateP_);
+        
+        if(userThreadStateP_.get()!=nullptr)
+        {
+          std::tie(friData,receive_ec,receive_bytes_transferred,send_ec,send_bytes_transferred) = *userThreadStateP_;
+          std::atomic_exchange(&std::get<latest_receive_monitor_state>(*userThreadStateP_),tempFriData);
+        }
+        else
+        {
+          std::tie(friData,receive_ec,receive_bytes_transferred,send_ec,send_bytes_transferred) = LatestState();
+        }
+    }
+
+    void destruct(){
+       m_shouldStop = true;
+       if(driver_threadP_){
+         driver_threadP_->join();
+       }
+    }
+    
+    ~KukaFRIClientDataDriver(){
+       destruct();
+    }
+
+    /// Is everything ok?
+    /// @return true if the kuka fri connection is actively running without any issues
+    /// @todo consider expanding to support real error codes
+    bool is_active()
+    {
+      return !exceptionPtr && isConnectionEstablished_;
+    }
+
+private:
+    /// Reads data off of the real kuka fri device in a separate thread
+    void update() {
+       try {
+            // initialize all of the states
+            latestStateP_ = make_shared_valid_LatestState();
+            nextStateP_ = make_shared_valid_LatestState();
+            userThreadStateP_ = make_shared_valid_LatestState();
+
+            // run the primary update loop in a separate thread
+            while (!m_shouldStop)
+            {
+                grl::robot::arm::update_state  (
+                                                *socketP_,
+                                                *std::get<latest_receive_monitor_state>(*nextStateP_),
+                                                std::get<latest_receive_ec>(*nextStateP_),
+                                                std::get<latest_receive_bytes_transferred>(*nextStateP_),
+                                                std::get<latest_send_ec>(*nextStateP_),
+                                                std::get<latest_send_bytes_transferred>(*nextStateP_)
+                                               );
+                isConnectionEstablished_ = true;
+                std::atomic_exchange(&latestStateP_,nextStateP_);
+            }
+           
+       } catch(...) {
+            // transport the exception to the main thread in a safe manner
+            exceptionPtr = std::current_exception();
+            m_shouldStop = true;
+       }
+       
+       isConnectionEstablished_ = false;
+    }
+
+    enum LatestStateIndex{
+     latest_receive_monitor_state,
+     latest_receive_ec,
+     latest_receive_bytes_transferred,
+     latest_send_ec,
+     latest_send_bytes_transferred
+    };
+    
+    typedef std::tuple<std::shared_ptr<KUKA::FRI::ClientData>,boost::system::error_code, std::size_t,boost::system::error_code, std::size_t>
+    LatestState;
+    
+    /// @todo num_dof should not be a magic number
+    /// @note the clientData will never be null
+    static LatestState make_LatestState(std::shared_ptr<KUKA::FRI::ClientData> clientData = std::make_shared<KUKA::FRI::ClientData>(KUKA::LBRState::NUM_DOF)){
+      return std::make_tuple(clientData,boost::system::error_code(), std::size_t(),boost::system::error_code(), std::size_t());
+    }
+    
+    static std::shared_ptr<LatestState> make_shared_valid_LatestState(std::shared_ptr<KUKA::FRI::ClientData> friData = std::make_shared<KUKA::FRI::ClientData>(KUKA::LBRState::NUM_DOF)){
+        
+        std::shared_ptr<LatestState> tempLatestStateP;
+        
+        if(friData.get()==nullptr)
+        {
+          tempLatestStateP = std::make_shared<LatestState>(make_LatestState());
+        }
+        else
+        {
+          tempLatestStateP = std::make_shared<LatestState>(make_LatestState(friData));
+        }
+        
+        return tempLatestStateP;
+    }
+    
+
+    /// @todo replace with unique_ptr
+    /// the latest state we have available
+    std::shared_ptr<LatestState> latestStateP_;
+    std::shared_ptr<LatestState> nextStateP_;
+    std::shared_ptr<LatestState> userThreadStateP_;
+    std::atomic<bool> m_shouldStop;
+    std::exception_ptr exceptionPtr;
+    std::atomic<bool> isConnectionEstablished_;
+
+    /// may be null, allows the user to choose if they want to provide an io_service
+    std::unique_ptr<boost::asio::io_service> optional_internal_io_service_P;
+    
+	// other things to do somewhere:
+	// - get list of control points
+	// - get the control point in the arm base coordinate system
+	// - load up a configuration file with ip address to send to, etc.
+	boost::asio::io_service& io_service_;
+    std::unique_ptr<boost::asio::ip::udp::socket> socketP_;
+    std::unique_ptr<std::thread> driver_threadP_;
+};
 
 
 
 
 
-
+/// @brief don't use this
 /// @todo replace with something generic
+/// @deprecated
 struct KukaState {
 	typedef boost::container::static_vector<double,KUKA::LBRState::NUM_DOF> joint_state;
     typedef boost::container::static_vector<double,6> cartesian_state;
@@ -165,7 +357,8 @@ struct KukaState {
 };
 
 
-
+/// @brief don't use this
+/// @deprecated
 void copy(const FRIMonitoringMessage& monitoringMsg, KukaState& state ){
     state.clear();
 	copy(monitoringMsg,std::back_inserter(state.position),revolute_joint_angle_open_chain_state_tag());
