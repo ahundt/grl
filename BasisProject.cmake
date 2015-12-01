48a59c01401705ec3e7c46c3efd4d6e95037bc38
Andrew Hundt <ATHundt@gmail.com>
2015-12-01 01:48:20 -0500

    UniversalRobots V-REP plugin initial implementation

--- BasisProject.cmake
+++ BasisProject.cmake
@@ -114,6 +114,7 @@ basis_project (
 	Eigen3             # Linear Algebra eigen.tuxfamily.com
     Nanopb             # Used in Kuka Fast Robot Interface
     FRI-Client-SDK_Cpp # Kuka Fast Robot Interface (FRI) Client SDK
+    ur_modern_driver # Universal Robots Device Driver https://github.com/ThomasTimm/ur_modern_driver/
     CisstNetlib # https://github.com/jhu-cisst/cisstNetlib  used for inverse kinematics plugin
     cisst{cisstNumerical,cisstOSAbstraction,cisstVector,cisstCommon} # used for inverse kinematics plugin
     sawConstraintController #  used for inverse kinematics plugin
