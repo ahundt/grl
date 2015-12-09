bfed46e0335475d7bbed12749173dbe5dac9f8a2
Andrew Hundt <ATHundt@gmail.com>
2015-12-08 23:13:28 -0500

    kinect2pclObjRecRansac.cpp now runs but produces junk results

--- test/kinect2pclObjRecRansac.cpp
+++ test/kinect2pclObjRecRansac.cpp
@@ -38,6 +38,10 @@ via Luigi Alamanni 13D, San Giuliano Terme 56010 (PI), Italy
 #include <pcl/features/integral_image_normal.h>
 
 
+#include "opencv2/surface_matching.hpp"
+#include <iostream>
+#include "opencv2/core/utility.hpp"
+
 using namespace pcl;
 using namespace pcl::io;
 using namespace pcl::console;
@@ -57,9 +61,199 @@ saveCloud (const std::string &filename, const Cloud &cloud, bool binary, bool us
   print_info ("[done, "); print_value ("%g", tt.toc ()); print_info (" ms : "); print_value ("%d", cloud.width * cloud.height); print_info (" points]\n");
 }
 
+
+
+  /**
+   * \breif convert an opencv collection of points to a pcl::PoinCloud, your opencv mat should have NAN's for invalid points.
+   * @param points3d opencv matrix of nx1 3 channel points
+   * @param cloud output cloud
+   * @param rgb the rgb, required, will color points
+   * @param mask the mask, required, must be same size as rgb
+   */
+  inline void
+  cvToCloudXYZRGBNormal(const cv::Mat& cvcloud, pcl::PointCloud<pcl::PointXYZRGBNormal>& cloud)
+  {
+    std::size_t rows = cvcloud.rows;
+    std::size_t cols = cvcloud.cols;
+    cloud.clear();
+  
+    for (std::size_t i = 0; i<rows; ++i) {
+      
+      pcl::PointXYZRGBNormal cp;
+      cp.x = cvcloud.at<float>(i,0);
+      cp.x = cvcloud.at<float>(i,1);
+      cp.x = cvcloud.at<float>(i,2);
+      if (cols > 3)
+      {
+        cp.normal_x = cvcloud.at<float>(i,3);
+        cp.normal_y = cvcloud.at<float>(i,4);
+        cp.normal_z = cvcloud.at<float>(i,5);
+      }
+      if (cols > 6)
+      {
+        cp.r = cvcloud.at<float>(i,6);
+        cp.g = cvcloud.at<float>(i,7);
+        cp.b = cvcloud.at<float>(i,8);
+      }
+      cloud.push_back(cp);
+    }
+  }
+
+
+std::vector<std::string> split(const std::string &text, char sep);
+
+std::vector<std::string> split(const std::string &text, char sep) {
+  std::vector<std::string> tokens;
+  std::size_t start = 0, end = 0;
+  while ((end = text.find(sep, start)) != std::string::npos) {
+    tokens.push_back(text.substr(start, end - start));
+    start = end + 1;
+  }
+  tokens.push_back(text.substr(start));
+  return tokens;
+}
+
+cv::Mat loadPLYSimple(const char* fileName, int withNormals = 0)
+{
+  cv::Mat cloud;
+  int numVertices=0;
+  int numCols=3;
+  bool with_color = false;
+  bool with_alpha = false; // alpha transparency
+
+  std::ifstream ifs(fileName);
+
+  if (!ifs.is_open())
+  {
+    printf("Cannot open file...\n");
+    return cv::Mat();
+  }
+
+  std::string str;
+  while (str.substr(0, 10) !="end_header")
+  {
+    std::vector<std::string> tokens = split(str,' ');
+    if (tokens.size() == 3 && tokens[0] == "element" && tokens[1] == "vertex")
+    {
+      numVertices = atoi(tokens[2].c_str());
+    }
+    if (tokens.size() ==3 && tokens[0] == "property")
+    {
+      if(tokens[3]=="nx" || tokens[3]=="normal_x" )
+      {
+        withNormals = true;
+        numCols+=3;
+      }
+      else if(tokens[3]=="r" || tokens[3]=="red" )
+      {
+        with_color = true;
+        numCols+=3;
+      }
+      else if(tokens[3]=="a" || tokens[3]=="alpha" )
+      {
+        with_alpha = true;
+        numCols+=1;
+      }
+    }
+    else if (tokens.size() > 1 && tokens[0] == "format")
+    {
+      if (tokens[1]!="ascii"){
+        printf("Cannot read file, only ascii ply format is currently supported...\n");
+        // uncomment below when CV_StsBadArg can be located
+        //OPENCV_ERROR (CV_StsBadArg, "loadPLYSimple", "Cannot read file, only ascii ply format is currently supported...");
+        return cv::Mat();
+      }
+    }
+    std::getline(ifs, str);
+  }
+
+  cloud=cv::Mat(numVertices, numCols, CV_32FC1);
+
+  for (int i = 0; i < numVertices; i++)
+  {
+    float* data = (float*)(&cloud.data[i*cloud.step[0]]);
+    for (int col = 0; col < numCols; ++col)
+    {
+      ifs >> data[col];
+    }
+  
+    if (withNormals)
+    {
+
+      // normalize to unit norm
+      double norm = sqrt(data[3]*data[3] + data[4]*data[4] + data[5]*data[5]);
+      if (norm>0.00001)
+      {
+        data[3]/=static_cast<float>(norm);
+        data[4]/=static_cast<float>(norm);
+        data[5]/=static_cast<float>(norm);
+      }
+    }
+  }
+
+  //cloud *= 5.0f;
+  return cloud;
+}
+
+pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr loadPLYSimpleCloud(const char* fileName, pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr model)
+{
+   cv::Mat cvcloud = loadPLYSimple(fileName);
+   cvToCloudXYZRGBNormal(cvcloud,*model);
+   return model;
+}
+
+
+bool parse (const std::string& filename)
+{
+  std::ifstream istream (filename.c_str (), std::ios::in | std::ios::binary);
+
+  std::string line;
+  std::size_t line_number_ = 0;
+
+  std::size_t number_of_format_statements = 0; 
+  std::size_t number_of_element_statements = 0; 
+  std::size_t number_of_property_statements = 0; 
+  std::size_t number_of_obj_info_statements = 0; 
+  std::size_t number_of_comment_statements = 0;
+
+  pcl::io::ply::format_type format = pcl::io::ply::unknown;
+  //std::vector< boost::shared_ptr<pcl::io::ply::element> > elements;
+
+  char line_delim = '\n';
+  int char_ignore_count = 0;
+
+  // magic
+  char magic[4];
+  istream.read (magic, 4);
+
+  // Check if CR/LF, setup delim and char skip
+  if (magic[3] == '\r')
+  {
+    istream.ignore (1);
+    line_delim = '\r';
+    char_ignore_count = 1;
+  }
+
+  ++line_number_;
+  if (!istream)
+  {
+    //if (error_callback_)
+    //  error_callback_ (line_number_, "parse error: couldn't read the magic string");
+    return false;
+  }
+
+  if ((magic[0] != 'p') || (magic[1] != 'l') || (magic[2] != 'y'))
+  {
+    //if (error_callback_)
+    //  error_callback_ (line_number_, "parse error: wrong magic string");
+    return false;
+  }
+  return true;
+}
+
 int main(int argc, char *argv[])
 {
-  std::cout << "Syntax is: " << argv[0] << " [-processor 0|1|2] [model.ply]\n -processor options 0,1,2 correspond to CPU, OPENCL, and OPENGL respectively\n";
+  std::cout << "Syntax is: " << argv[0] << " [--processor 0|1|2] [model.ply]\n --processor options 0,1,2 correspond to CPU, OPENCL, and OPENGL respectively\n";
   processor freenectprocessor = OPENGL;
   std::vector<int> ply_file_indices;
   
@@ -89,30 +283,40 @@ int main(int argc, char *argv[])
       pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr model(new pcl::PointCloud<pcl::PointXYZRGBNormal>());
       // these are the point clouds of the ply files read in
       pcl::PointCloud<Normal>::Ptr modelnormal(new pcl::PointCloud<pcl::Normal>());
-      pcl::copyPointCloud(*model,*modelnormal);
       pcl::PointCloud<pcl::PointXYZ>::Ptr modelxyz(new pcl::PointCloud<pcl::PointXYZ>());
+      
+      std::string modelFile(argv[idx]);
+
+      bool success = parse(modelFile);
+      std::cout << "success: " << success << "\n";
+      reader.read(modelFile,*model);
+      //loadPLYSimpleCloud(modelFile.c_str(),model);
+      pcl::copyPointCloud(*model,*modelnormal);
       pcl::copyPointCloud(*model,*modelxyz);
       
-      std::string modelFile(argv[ply_file_indices[idx]]);
-
-      //we need to map these strings, which are the names, to the ply files
-      pclMap[modelFile] = model;
-
-      reader.read(modelFile,*model);
       /// @todo should const_cast really be used?
       orransac.addModel(*modelxyz,*modelnormal,modelFile);
       
+
+      //we need to map these strings, which are the names, to the ply files
+      pclMap[modelFile] = model;
   }
   
     // estimate normals http://pointclouds.org/documentation/tutorials/normal_estimation_using_integral_images.php#normal-estimation-using-integral-images
-    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
+    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>());
 
   // setup kinect
   //changed cloud type to ::Ptr
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud;
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr kinect_cloud;
   K2G k2g(freenectprocessor);
   std::cout << "getting cloud" << std::endl;
-  cloud = k2g.getCloud();
+  kinect_cloud = k2g.getCloud();
+
+  kinect_cloud->sensor_orientation_.w() = 0.0;
+  kinect_cloud->sensor_orientation_.x() = 1.0;
+  kinect_cloud->sensor_orientation_.y() = 0.0;
+  kinect_cloud->sensor_orientation_.z() = 0.0;
 
   cloud->sensor_orientation_.w() = 0.0;
   cloud->sensor_orientation_.x() = 1.0;
@@ -130,8 +334,8 @@ int main(int argc, char *argv[])
     using namespace std::chrono;
     static high_resolution_clock::time_point last;
 
-    auto tnow = high_resolution_clock::now();   
-    cloud = k2g.updateCloud(cloud);
+    auto tnow = high_resolution_clock::now();
+    kinect_cloud = k2g.updateCloud(kinect_cloud);
     auto tpost = high_resolution_clock::now();
     std::cout << "delta " << duration_cast<duration<double>>(tpost-tnow).count()*1000 << std::endl;
     
@@ -142,11 +346,12 @@ int main(int argc, char *argv[])
     /// @todo make magic numbers into params
     ne.setMaxDepthChangeFactor(1.0f);
     ne.setNormalSmoothingSize(10.0f);
-    ne.setInputCloud(cloud);
+    ne.setInputCloud(kinect_cloud);
     ne.compute(*normals);
     
+    pcl::copyPointCloud(*kinect_cloud,*cloud);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloudxyz(new pcl::PointCloud<pcl::PointXYZ>());
-    pcl::copyPointCloud(*cloud,*cloudxyz);
+    pcl::copyPointCloud(*kinect_cloud,*cloudxyz);
     std::list< pcl::recognition::ObjRecRANSAC::Output > recognized_objects;
     /// @todo make magic numbers into params
     double success_probability=0.99;
@@ -186,12 +391,13 @@ int main(int argc, char *argv[])
     pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
     viewer->updatePointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud");
 
-    if(ply_file_indices.size() > 0 ){
-        pcl::PCLPointCloud2 cloud2;
-        pcl::toPCLPointCloud2(*cloud,cloud2);
-        saveCloud("cloudname.ply",cloud2,false,false);
-        done = true;
-    }
+    /// @todo support writing to file again
+//    if(ply_file_indices.size() > 0 ){
+//        pcl::PCLPointCloud2 cloud2;
+//        pcl::toPCLPointCloud2(*cloud,cloud2);
+//        saveCloud("cloudname.ply",cloud2,false,false);
+//        done = true;
+//    }
   }
 
   k2g.shutDown();
