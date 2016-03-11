a6ea6d57c2e2c8f214093a2691059cdb41dcd4ad
Andrew Hundt <ATHundt@gmail.com>
2016-03-11 18:08:51 -0500

    print an error if roscore isn't running

--- src/ros/grl_kuka_ros_driver.cpp
+++ src/ros/grl_kuka_ros_driver.cpp
@@ -6,6 +6,11 @@ using namespace grl::ros;
 int main(int argc, char **argv) {
 
   ros::init(argc,argv,"kuka_lbr_ros_bridge");
+   
+  if(!ros::master::check())
+  {
+    std::cerr << "WARNING: roscore does not appear to be running\n";
+  }
 
   std::shared_ptr<KukaLBRiiwaROSPlugin> plugin(std::make_shared<KukaLBRiiwaROSPlugin>());
   plugin->construct();
