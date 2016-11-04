8b058dfa0b91de5fcc6b3f75b28fb40c8204db90
Andrew Hundt <ATHundt@gmail.com>
2016-11-04 08:48:20 -0400

    INVERT ROTATION COMPONENT OF SpaceVecAlg Ptransformd

--- include/grl/vrep/SpaceVecAlg.hpp
+++ include/grl/vrep/SpaceVecAlg.hpp
@@ -12,23 +12,31 @@
 /// @param objectHandle The V-Rep object handle for which the transform is needed
 /// @param relativeToObjectHandle The frame of reference in which to get the object handle, -1 (the default) gets the absolute position aka world frame
 /// for constructor see: https://github.com/jrl-umi3218/SpaceVecAlg/blob/master/src/PTransform.h
+/// @todo TODO(ahundt) FIXME Be careful! the quaternion component is inverted from V-REP, perhaps move out or create a separate version?
 sva::PTransform<double> getObjectPTransform(int objectHandle, int relativeToObjectHandle = -1)
 {
+
+/// @todo TODO(ahundt) FIXME Be careful! the quaternion component is inverted from V-REP, perhaps move out or create a separate version?
   std::pair<Eigen::Quaterniond,Eigen::Vector3d> baseQuatTransformPair = getObjectTransformQuaternionTranslationPair(objectHandle, relativeToObjectHandle);
-  sva::PTransform<double> ptransform(baseQuatTransformPair.first,baseQuatTransformPair.second);
+  sva::PTransform<double> ptransform(baseQuatTransformPair.first.inverse(),baseQuatTransformPair.second);
   return ptransform;
 }
 
+
+/// @todo TODO(ahundt) FIXME Be careful! the quaternion component is inverted from V-REP, perhaps move out or create a separate version?
 sva::PTransform<double> eigenAffineToPtransform(const Eigen::Affine3d& eigenTransform)
 {
-    return sva::PTransform<double>(eigenTransform.rotation(),eigenTransform.translation());
+/// @todo TODO(ahundt) FIXME Be careful! the quaternion component is inverted from V-REP, perhaps move out or create a separate version?
+    return sva::PTransform<double>(eigenTransform.rotation().inverse(),eigenTransform.translation());
 }
 
+/// @todo TODO(ahundt) FIXME Be careful! the quaternion component is inverted from V-REP, perhaps move out or create a separate version?
 Eigen::Affine3d PTranformToEigenAffine(sva::PTransform<double> & ptransform)
 {
+/// @todo TODO(ahundt) FIXME Be careful! the quaternion component is inverted from V-REP, perhaps move out or create a separate version?
     Eigen::Affine3d eigenTransform;
     eigenTransform.translation() = ptransform.translation();
-    eigenTransform.matrix().block<3,3>(0,0) = ptransform.rotation();
+    eigenTransform.matrix().block<3,3>(0,0) = ptransform.rotation().inverse();
     return eigenTransform;
 }
 
